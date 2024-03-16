ac399e4a721235c298989c6e112ae4847e5b33cb
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-03-15 21:10:09 -0400

    feat: adds secret validation

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 import express from 'express';
+import * as crypto from "crypto";
 
 const app = express();
 
-app.post('/github/webhook', express.json({type: 'application/json'}), async (request, response) => {
-  response.status(202).send('Accepted');
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  const githubEvent = request.headers['x-github-event'];
-  const secret = request.headers['X-Hub-Signature-256'];
+const verify_signature = (req) => {
+  const signature = crypto
+    .createHmac("sha256", WEBHOOK_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+  let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+  let untrusted =  Buffer.from(req.headers.get("x-hub-signature-256"), 'ascii');
+  return crypto.timingSafeEqual(trusted, untrusted);
+};
 
-  console.log('GitHub event: ', githubEvent);
-  console.log('GitHub secret: ', secret);
+app.post('/github/webhook', express.json({type: 'application/json'}), async (req, res) => {
+  if (!verify_signature(req)) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  res.status(202).send('Accepted');
+
+  const githubEvent = req.headers['x-github-event'];
+  const secret = req.headers['X-Hub-Signature-256'];
+
+  if (githubEvent === 'push') {
+    console.log('GitHub event: ', githubEvent);
+    console.log('GitHub secret: ', secret);
+  } else {
+    console.log(`Invalid github event received: ${githubEvent}`);
+  }
 });
-
-// app.post('/webhook', express.json({type: 'application/json'}), async (request, response) => {
-//   response.status(202).send('Accepted');
-
-//   const githubEvent = request.headers['x-github-event'];
-
-//   console.log('Request: ', request);
-//   console.log('GitHub event: ', githubEvent);
-
-//   if (githubEvent === 'issues') {
-//     const data = request.body;
-//     const action = data.action;
-//     if (action === 'opened') {
-//       console.log(`An issue was opened with this title: ${data.issue.title}`);
-//     } else if (action === 'closed') {
-//       console.log(`An issue was closed by ${data.issue.user.login}`);
-//     } else {
-//       console.log(`Unhandled action for the issue event: ${action}`);
-//     }
-//   } else if (githubEvent === 'ping') {
-//     console.log('GitHub sent the ping event');
-//   } else {
-//     console.log(`Unhandled event: ${githubEvent}`);
-//   }
-// });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server ready and waiting on port ${process.env.PORT}`);
