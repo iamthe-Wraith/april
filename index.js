531e791a9f0bbdca03cd4564fde0a59cfb18a8d1
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-03-16 20:23:30 -0400

    refactor: reduces dependency on github specific functionality.

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ import { execFile } from 'child_process';
 
 const app = express();
 
-const verify_signature = (req) => {
-  const signature = crypto
-    .createHmac("sha256", process.env.WEBHOOK_SECRET)
-    .update(JSON.stringify(req.body))
-    .digest("hex");
-  let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
-  let untrusted = Buffer.from(req.headers['x-hub-signature-256'], 'ascii');
-  return crypto.timingSafeEqual(trusted, untrusted);
-};
-
 const execute = () => execFile(
   `bash`, 
   [
@@ -31,28 +21,52 @@ const execute = () => execFile(
   }
 );
 
-app.post('/github/webhook', express.json({type: 'application/json'}), async (req, res) => {
+/**
+ * this function is dependent on the structure of the github webhook event,
+ * but should return a non-github dependent object that can be used to determine
+ * if the event is the correct event to act on. 
+ */
+const getAction = (req) => {
+  let action; // values: 
+  
+  if (req.headers['x-github-event'] === 'workflow_run' && req.body.action === 'completed' && req.body.workflow.name === 'Publish Docker Image') {
+    action = 'deploy'
+  }
+
+  return action;
+}
+
+const verify_signature = (req) => {
+  const signature = crypto
+    .createHmac("sha256", process.env.WEBHOOK_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+  let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+  let untrusted = Buffer.from(req.headers['x-hub-signature-256'], 'ascii');
+  return crypto.timingSafeEqual(trusted, untrusted);
+};
+
+app.post('/deploy/webhook', express.json({type: 'application/json'}), async (req, res) => {
   try {
     if (!verify_signature(req)) {
       res.status(401).send("I'm a very rich widow with a terrible secret...but that's not the secret.");
       return;
     }
   
-    const githubEvent = req.headers['x-github-event'];
-  
-    if (githubEvent === 'workflow_run') {
-      if (req.body.action === 'completed' && req.body.workflow.name === 'Publish Docker Image') {
+    const action = getAction(req);
+
+    switch (action) {
+      case 'deploy':
         res
           .status(202)
           .send('Fine, I\'ll do it. But I\'m not happy about it...and I\'m going to tell everyone you hide your own easter eggs.');
   
         execute();
-      }
-    } else {
-      res.status(422).send('Unprocessable');
-  
-      console.log(`${githubEvent} isn't the right github event. I like telling you because I want you're happiness to go away.`);
-      console.log(req.body);
+        break;
+      default:
+        const msg = 'Yeeeah, I\'m not doing that. I like telling you because I want you\'re happiness to go away.';
+        res.status(422).send(msg);
+        console.log(msg, action, req.headers, req.body);
     }
   } catch (err) {
     res.status(500).send('I have no idea what I\'m doing.');
